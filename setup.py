fcedc61166de38d947110ddbd101d89e161445ca
Nicholas-scuola <56036434+nicholas-progetti-scuola@users.noreply.github.com>
2020-11-08 17:22:15 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,88 @@
+import os
+import  subprocess
+from tkinter import messagebox
+import tkinter
+
+
+import tkinter as tk 
+ 
+class Inputbox():
+    def __init__(self, text=""):
+        self.root = tk.Tk()
+        self.get = ""
+        self.root.geometry("300x100")
+        self.root.title("Inputbox")
+        self.label_file_name = tk.Label(self.root, text=text)
+        self.label_file_name.pack()
+        self.entry = tk.Entry(self.root,show="*")
+        self.entry.pack()
+        self.entry.focus()
+        self.entry.bind("<Return>", lambda x: self.getinput(self.entry.get()))
+        self.root.mainloop()
+ 
+    def getinput(self, value):
+        self.get = value
+        self.root.destroy()
+ 
+ 
+
+
+
+root = tkinter.Tk()
+root.withdraw()
+scelta=messagebox.askyesno("installer","do you want continue to install?")
+root.destroy()
+
+
+
+if scelta is False:
+    exit()
+
+
+try:
+    from cryptography.fernet import Fernet
+    print("ok lib Fernet is alredy install")
+
+except ImportError:
+    os.system("pip install cryptography")
+    input("open again")
+    exit()
+
+
+if os.path.exists("lib\\security.py") is False or os.path.exists("lib\\Crittografia.py") is False:
+    input("directory lib non trovato")
+    exit()
+
+if os.path.exists("lib\\tabella.py") is False or os.path.exists("lib\\punteggio.py") is False:
+    input("directory lib non trovato")
+    exit()
+
+if os.path.exists("lib\\delete_line .py") is False or os.path.exists("lib\\gestici.py") is False:
+    input("directory lib non trovato")
+    exit()
+
+
+print("ok")
+    
+
+
+password=Inputbox("insert master password")
+
+os.chdir("lib")
+posizione=os.getcwd()
+scrivi=open("password.py","w+")
+scrivi.write("""
+def password():
+    password='"""+password.get+"""'"""
+    +"""\n    return password""")
+scrivi.close()
+os.system("python -m compileall -b password.py")
+os.remove("password.py")
+
+os.system("cls")
+
+
+#subprocess.Popen("python -c \"import os, time; time.sleep(1); os.remove('{}');\"".format(sys.argv[0]))
+
+
+input("premi un tasto per finire")
